e157cc86215121ddb9d4dfa69ffeab852929b141
ANgiang19 <andyngiang@Andys-MacBook-Pro-2.local>
2017-10-17 17:53:22 +0800

    Odd Or Even
    Ask the user for a number. Depending on whether the number is even or
    odd, print out an appropriate message to the user.

---
+++ oddeven.py
@@ -0,0 +1,8 @@
+# Odd or Even
+user_num = int(input("Input an integer\n"))
+check_num = user_num % 2
+
+if check_num == 0:
+    print("This an even number..")
+else:
+    print("This is an odd number..")
